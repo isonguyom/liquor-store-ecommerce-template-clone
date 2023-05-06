72c23a99b3a06d7d456ee7bf4fbb59d2a4688906
isonguyom <isonguyom@gmail.com>
2023-05-06 16:32:39 +0100

    add build config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/liquor-store-ecommerce-template-clone/",
   plugins: [vue()],
   resolve: {
     alias: {
